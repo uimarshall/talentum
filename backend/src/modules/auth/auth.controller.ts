7d0bda0a558aa4d4b777cb263fb3bd84897cd5c4
Marshall Akpan <uimarshall@gmail.com>
2025-04-06 21:19:43 +0100

    Add login functionality

--- backend/src/modules/auth/auth.controller.ts
+++ backend/src/modules/auth/auth.controller.ts
@@ -3,13 +3,18 @@ import asyncHandler from 'express-async-handler';
 import { asyncErrorHandler } from '../../middlewares/asyncErrorHandler';
 import { AuthService } from './auth.service';
 import { HTTPSTATUS } from '../../config/http.config';
-import { registerSchema } from '../../shared/validators/auth.validator';
+import { loginSchema, registerSchema } from '../../shared/validators/auth.validator';
 
 export class AuthController {
   private authService: AuthService;
   constructor(authService: AuthService) {
     this.authService = authService;
   }
+
+  // Register user
+  // @desc Register user
+  // @route POST /api/auth/register
+  // @access Public
 
   public register = asyncHandler(async (req: Request, res: Response): Promise<any> => {
     const userAgent = req.headers['user-agent']; // The user agent is a string that identifies the client software making the request.
@@ -20,6 +25,21 @@ export class AuthController {
     return res.status(HTTPSTATUS.CREATED).json({
       message: 'User registered successfully!, please check your email to verify your account',
       data: user, // Return the user data in the response.
+    });
+  });
+
+  // Login user
+  // @desc Login user
+  // @route POST /api/auth/login
+  // @access Public
+  public login = asyncHandler(async (req: Request, res: Response): Promise<any> => {
+    const userAgent = req.headers['user-agent'];
+    // const ipAddress = req.ip;
+    const body = loginSchema.parse({ ...req.body, userAgent }); // Validate the request body against the schema.
+    const { user, accessToken, refreshToken, mfaRequired } = await this.authService.login(body); // Call the login method of the AuthService with the validated body.
+    return res.status(HTTPSTATUS.OK).json({
+      message: 'User logged in successfully',
+      data: { user, mfaRequired }, // Return the user data and token in the response.
     });
   });
 }
